9762b744c210accb2cb942f8ad37781f7fa06107
masashiabe <tech.abe.jp@gmail.com>
2021-04-13 18:04:21 +0900

    postal code

---
+++ postalCode.php
@@ -0,0 +1,16 @@
+<?php
+
+$postalCode = "123-4567";
+
+function checkPostalCode ($str) {
+$replaced = str_replace("-", "", $str);
+$length = strlen($replaced);
+
+if ($length === 7) {
+    return true;
+} else {
+    return false;
+}
+}
+
+var_dump(checkPostalCode($postalCode));
