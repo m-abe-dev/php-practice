1c0eb0b3a8a85c7c77cf5722884f521f2d4a0a7c
masashiabe <tech.abe.jp@gmail.com>
2021-04-14 10:08:06 +0900

    require php

---
+++ common.php
@@ -0,0 +1,7 @@
+<?php
+
+$commonVariable = "this is a common variable";
+
+function commonTest() {
+    echo "this file is out of an another file";
+}
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+
+// 階層が違う場合はpassを書く
+require ("common.php");
+
+echo ($commonVariable);
+commonTest();
+echo __DIR__;
+echo __FILE__;
