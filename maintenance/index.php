22e593d030755069cbfdb5977bd508187bfa87bb
masashiabe <tech.abe.jp@gmail.com>
2021-04-29 23:40:15 +0900

    PDO トランザクション

--- maintenance/index.php
+++ maintenance/index.php
@@ -32,3 +32,23 @@ $result = $stmt->fetchall();
 echo "<pre>";
 var_dump($result);
 echo "</pre>";
+
+
+// トランザクション まとまって処理 beginTransaction, commit, rollback
+// ex)銀行 残高を確認->Aさんから引き落とし->Bさんに振り込み
+
+$pdo->beginTransaction();
+
+try {
+    // SQL処理
+    $stmt = $pdo->prepare($sql);
+    // 紐付けと型を指定
+    $stmt->bindValue("id", 3, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $pdo->commit();
+} catch (PDOException $e) {
+    // 更新のキャンセル
+    $pdo->rollback();
+}
+
